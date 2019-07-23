42976ebafaf2e39e4b0307a40ad6f2ab59bdf190
NG Chester <zhentingchester.ng@murex.com>
2019-07-23 15:37:30 +1000

    Adding an improved version of the function that eliminates the need for a concat at the end

--- recursion/pure_recursion.js
+++ recursion/pure_recursion.js
@@ -14,4 +14,17 @@ function collectOddValues(arr) {
 
 }
 
+// This one is way faster because we don't do a concat for every single iteration
+function collectOddValues2(arr, res = []) {
+	if (!arr.length) return res;
+	if (arr[0] % 2 !== 0) res.push(arr[0]);
+
+	return collectOddValues2(arr.slice(1), res);
+}
+console.time('A')
 console.log(collectOddValues([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+console.timeEnd('A')
+
+console.time('B')
+console.log(collectOddValues2([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+console.timeEnd('B')
